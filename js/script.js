52dbf7b91dafd88e194effdcba333301296e9094
oliverjauregui <oliver.jauregui20@gmail.com>
2018-11-09 00:54:02 -0500

    add design

--- js/script.js
+++ js/script.js
@@ -20,4 +20,37 @@ $(document).ready(function(){
 
 	});
 
+	$('.owl-carousel').owlCarousel({
+		loop:true,
+		margin: 10,
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:3
+			},
+			1000:{
+				items:5
+			}
+		}
+	})
+	$('.owl-carousel2').owlCarousel({
+		loop:true,
+		margin: 10,
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			1000:{
+				items:1
+			}
+		}
+	})
+
 });
